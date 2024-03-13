2d7655e89031f0e8ff2dda0a00b33e4fec131963
Documenter.jl <documenter@juliadocs.github.io>
2024-03-13 10:46:57 +0000

    build based on 7279541

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = ClimFluids","category":"page"},{"location":"#ClimFluids","page":"Home","title":"ClimFluids","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for ClimFluids.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [ClimFluids]","category":"page"}]
+}
