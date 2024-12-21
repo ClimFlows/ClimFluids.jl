25232f98088a548984dc7febd555e5beb46680a4
Thomas Dubos <thomas.dubos@polytechnique.edu>
2024-12-21 18:40:46 +0100

    Fix fallback implementation

--- src/julia/perfectgas.jl
+++ src/julia/perfectgas.jl
@@ -4,7 +4,7 @@ abstract type PerfectGas{F} <: AbstractFluid{F,1} end
 
     temperature(gas::PerfectGas, (p,v)::PV) = p*v*inv(gas.R)
     specific_volume(gas::PerfectGas, (p,T)::PT) = gas.R*T*inv(p)
-    specific_internal_energy(gas::PerfectGas, (p,T)::PT) = specific_enthalpy(gas,p,T) - gas.R*T
+    specific_internal_energy(gas::PerfectGas, (p,T)::PT) = specific_enthalpy(gas, (;p,T)) - gas.R*T
     function sound_speed2(gas::PerfectGas, (p,T)::PT)
         (R, Cp) = (gas.R, heat_capacity(gas, (;p,T)))
         return (Cp * inv(Cp-R) * R) * T
